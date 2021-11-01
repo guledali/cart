0f5d140779421baf7629c8b799fe95586c1eff6c
guledali <guled.ali01@gmail.com>
2021-11-01 08:47:56 +0100

    Forgot testing list_item

--- lib/product.rb
+++ lib/product.rb
@@ -6,14 +6,4 @@ class Product
     @name = name
     @price = price
   end
-
-  # def find(name)
-  #   if @products.empty?
-  #     raise StandardError.new "Our products are empty at momment"
-  #   end
-
-  #   product = @products.find { |x| x["name"] == name }
-  #   @product.push(product.transform_keys(&:to_sym))
-  #   return @product
-  # end
 end
--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -1,10 +1,22 @@
 require("rspec")
 require_relative("../lib/line_item.rb")
+require_relative("../lib/product.rb")
 
 RSpec.describe "testing line_item" do
   before do
+    @product = Product.new(1, "jacket", 300)
+    @line_item = LineItem.new(@product, 1)
   end
 
-  it "" do
+  it "should increase the quantity" do
+    @line_item.increase
+
+    expect(@line_item.quanitity).to eq(2)
+  end
+
+  it "should decrease the quantity" do
+    @line_item.decrease
+
+    expect(@line_item.quanitity).to eq(0)
   end
 end
